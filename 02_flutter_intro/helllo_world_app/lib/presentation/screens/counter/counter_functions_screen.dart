5aa8d8e001eced466dc98cd4328e3a6106482569
alanmoren0D <112904478+alanmoren0D@users.noreply.github.com>
2023-08-31 08:19:32 -0600

    creacion de widgets

--- 02_flutter_intro/helllo_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
+++ 02_flutter_intro/helllo_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -7,74 +7,89 @@ class CounterFunctionScreen extends StatefulWidget {
   State<CounterFunctionScreen> createState() => _CounterFunctionScreenState();
 }
 
-class _CounterFunctionScreenState extends State<CounterFunctionScreen> 
-{
-  int clickCounter=0;
-  String clickWordState='Click';
+class _CounterFunctionScreenState extends State<CounterFunctionScreen> {
+  int clickCounter = 0;
+  String clickWordState = 'Click';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('CounterFunctionApp'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_rounded), 
-            onPressed: () {  
-              setState(() 
-              {
-                clickCounter = 0;
-              });
-            },
-
-          ),
-        ],
-      ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('$clickCounter', style: const TextStyle(
-                  fontSize: 150, 
-                  fontWeight: FontWeight.w400
-                )
-              ),
-              Text(clickWordState,style: const TextStyle(
-                  fontSize: 30, 
-                  fontWeight: FontWeight.w100
-                ),
-              )
-            ],
-          )
+        appBar: AppBar(
+          title: const Text('CounterFunctionApp'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            ),
+          ],
         ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 150, fontWeight: FontWeight.w400)),
+            Text(
+              clickCounter == 0 ? 'clicks' : 'click',
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w100),
+            )
+          ],
+        )),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              onPressed: ()
-              {
-                setState(() 
-                {
-                  clickCounter++;
-                  clickWordState = clickCounter >9 ? 'Clicks' : 'Click';   
+            CustomActionButton(
+              icon: Icons.refresh_outlined,
+              onPresed: () {
+                setState(() {
+                  clickCounter = 0;
                 });
               },
-              child: const Icon(Icons.plus_one),
             ),
             const SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: ()
-              {
-                setState(() 
-                {
-                  clickCounter--;
-                  clickWordState = clickCounter >9 ? 'Clicks' : 'Click';   
+            CustomActionButton(
+              icon: Icons.plus_one,
+              onPresed: () {
+                setState(() {
+                  clickCounter++;
                 });
               },
-              child: const Icon(Icons.exposure_minus_1_outlined),
+            ),
+            const SizedBox(height: 10),
+            CustomActionButton(
+              icon: Icons.exposure_minus_1_outlined,
+              onPresed: () {
+                setState(() {
+                  if (clickCounter == 0) return;
+                  clickCounter--;
+                });
+              },
             )
           ],
-        )
-      );
+        ));
+  }
+}
+
+class CustomActionButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPresed;
+
+  const CustomActionButton({
+    super.key,
+    required this.icon,
+    this.onPresed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPresed,
+      child: Icon(icon),
+    );
   }
 }
