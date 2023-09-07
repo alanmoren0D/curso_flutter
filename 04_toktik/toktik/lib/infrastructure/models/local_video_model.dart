5329d4337177577a6500da7c8fdede72af13c7fd
alanmoren0D <112904478+alanmoren0D@users.noreply.github.com>
2023-09-07 17:17:00 -0600

    scroll vertical y otras cosas

--- 04_toktik/toktik/lib/infrastructure/models/local_video_model.dart
+++ 04_toktik/toktik/lib/infrastructure/models/local_video_model.dart
@@ -16,14 +16,14 @@ class LocalVideoModel {
 
     factory LocalVideoModel.fromJson(Map<String, dynamic> json) => LocalVideoModel(
         name: json["name"] ?? 'No name',
-        videoUrl: json["videoUlr"],
+        videoUrl: json["videoUrl"],
         likes: json["likes"] ?? 0,
         views: json["views"] ?? 0,
     );
 
     Map<String, dynamic> toJson() => {
         "name": name,
-        "videoUlr": videoUrl,
+        "videoUrl": videoUrl,
         "likes": likes,
         "views": views,
     };
--- 04_toktik/toktik/lib/presentation/provider/discover_provider.dart
+++ 04_toktik/toktik/lib/presentation/provider/discover_provider.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier
 {
@@ -10,9 +12,15 @@ class DiscoverProvider extends ChangeNotifier
   List<VideoPost> videos = [];
 
 
-Future<void> loadNextPage() async
-{
-  
-}
+  Future<void> loadNextPage() async
+  {
+    
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts.map((video )=> LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
+    notifyListeners();
+  }
 
 }
