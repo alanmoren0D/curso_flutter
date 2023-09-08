a3f5ba87ec933829d3435154b2151f1ff47ec034
alanmoren0D <112904478+alanmoren0D@users.noreply.github.com>
2023-09-08 17:09:17 -0600

    hasta que dio el error :(

--- 04_toktik/toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ 04_toktik/toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
 
@@ -12,13 +14,32 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue,)
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index)
+      {
+        final VideoPost videoPost = videos[index];
+        // print('${videoPost.videoUrl}------------------------------------------------------------------------------------');
+        return Stack(
+          children: 
+          [
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              )
+            ),
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            )
+          ],
+        );
+
+      },
     );
   }
 }
