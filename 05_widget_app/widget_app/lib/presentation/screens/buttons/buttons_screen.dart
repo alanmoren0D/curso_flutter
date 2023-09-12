a272301a77940e3e4c07fe007b69d021513e52b6
alanmoren0D <112904478+alanmoren0D@users.noreply.github.com>
2023-09-12 17:19:40 -0600

    lección de rutas

---
+++ 05_widget_app/widget_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ButtonsScreen extends StatelessWidget {
+
+  static const String name = 'buttons_screens';
+
+  const ButtonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Buttons screen"),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
